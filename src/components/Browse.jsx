ce35160922a351359be5f33642b57d84b9d3f384
kunalthakur-17 <kunalkharyal@gmail.com>
2026-05-23 18:30:24 +0530

    add MovieList api

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,15 +1,48 @@
-import Header from "./Header";
-import MainContainer from "./MainContainer";
-import MovieContainer from "./MovieContainer";
+import  { useEffect } from 'react'
+import Header from './Header';
+import { useSelector} from "react-redux";
+import { useNavigate } from "react-router-dom";
+import MainContainer from './MainContainer';
+import MovieContainer from './MovieContainer';
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
+import usePopularMovies from '../hooks/usePopularMovies';
+import useTopRatedMovies from '../hooks/useTopRatedMovies';
+import useUpcomingMovies from '../hooks/useUpcomingMovies';
+import SearchMovie from './SearchMovie';
 
-export default function Browse() {
-  return (
-    <div className="bg-black min-h-screen">
-      <Header />
-      <MainContainer />
-      <div className="px-6">
-        <MovieContainer />
-      </div>
-    </div>
-  );
+const Browse = () => {
+    const user = useSelector(store => store.app.user);
+    const toggle = useSelector(store => store.movie.toggle);
+    const navigate = useNavigate();
+
+    // my custom hooks
+    useNowPlayingMovies();
+    usePopularMovies();
+    useTopRatedMovies();
+    useUpcomingMovies();
+
+    useEffect(() => {
+        if (!user) {
+            navigate("/");
+        }
+    }, []);
+    return (
+        <div >
+            <Header />
+            <div>
+                {
+                    toggle ? <SearchMovie /> : (
+                        <>
+                            <MainContainer />
+                            <MovieContainer />
+                        </>
+
+                    )
+                }
+
+            </div>
+        </div>
+    )
 }
+
+export default Browse
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,12 +1,20 @@
-import VideoBackGround from "./VideoBackGround";
-import VideoTitle from "./VideoTitle";
+import React from 'react'
+import VideoTitle from './VideoTitle'
+import VideoBackground from './VideoBackground';
+import {useSelector} from "react-redux";
 
-export default function MainContainer() {
+const MainContainer = () => {
+  const movie = useSelector(store=>store.movie?.nowPlayingMovies);
+  if(!movie) return; // early return in react
+ 
+  const {overview, id, title} = movie[4];
+  
   return (
-    <div className="relative w-full h-[85vh] overflow-hidden">
-      <VideoTitle />
-      <VideoBackGround/>
-
+    <div>
+        <VideoTitle title={title} overview={overview}/>
+        <VideoBackground movieId={id}/>
     </div>
-  );
+  )
 }
+
+export default MainContainer
